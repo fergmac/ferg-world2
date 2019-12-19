aea754282440f56dc44069b3ab18b4ec88cd74fd
fergmac <f.macconnell@gmail.com>
2019-12-18 21:35:48 -0800

    Split Homepage up into smaller components and add styles for menu-item

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Homepage from './homepage.component.jsx';
+import Homepage from './pages/homepage/homepage.component';
 
 function App() {
   return (
