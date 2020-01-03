f7ce0d6090056e5e4cdf9adafebd63bb19100764
Fergus MacConnell <fergus.macconnell@widerfunnel.com>
2020-01-03 15:29:39 -0800

    Add redux

---
+++ src/redux/root-reducer.js
@@ -0,0 +1,8 @@
+// this combines all our states together
+import { combineReducers } from 'redux';
+import userReducer from './user/user.reducer';
+
+export default combineReducers({
+	user: userReducer
+});
+
