f01ee13a6d2a41c6b36a494aa96cf7e85e595657
Fergus MacConnell <fergus.macconnell@widerfunnel.com>
2020-01-07 16:44:32 -0800

    Build out checkout page: in progress

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -9,12 +9,19 @@ export const selectCartItems = createSelector(
 
 export const selectCartHidden = createSelector(
 	[selectCart],
-	cart => cart.cartHidden
+	cart => cart.hidden
 )
 
 export const selectCartItemsCount = createSelector(
 	[selectCartItems],
 	cartItems => cartItems.reduce(
 		(accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity, 0
+	)
+)
+
+export const selectCartTotal = createSelector(
+	[selectCartItems],
+	cartItems => cartItems.reduce(
+		(accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity * cartItem.price, 0
 	)
 )
