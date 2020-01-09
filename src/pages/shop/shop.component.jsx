8c5f59d12804073dfac09deff0f2984ddfafa33a
Fergus MacConnell <fergus.macconnell@widerfunnel.com>
2020-01-09 14:27:45 -0800

    Moved directory and shop data into own reducer, created respective selectors and updated corresponding components with redux flow for directory and shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,16 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-import SHOP_DATA from './shop.data.js';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../../pages/collection/collection.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
-
-class ShopPage extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-	}
-	
-	render() {
-        const {collections} = this.state;
-		return (
-            <div className='shop-page'>
-            {
-                collections.map(({id, ...OtherCollectionProps}) => (
-                    <CollectionPreview key={id} {...OtherCollectionProps} />
-                ))
-            }
-            </div>
-        );
-	}
+const ShopPage = ({ match }) => {
+    return (
+        <div className='shop-page'>
+            <Route exact path={`${match.path}`} component={CollectionsOverview} />
+            <Route path={`${match.path}/:categoryId`} component={CollectionPage} />
+        </div>
+    );
 }
 
 export default ShopPage;
