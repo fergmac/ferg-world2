574aa551bc77b3c46bb26c6146249307f9cb6f21
Fergus MacConnell <f.macconnell@gmail.com>
2020-02-04 21:46:51 -0800

    Add withSpiner HOC

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		const { setCurrentUser} = this.props;
+		const { setCurrentUser } = this.props;
 		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 			// this.setState({ currentUser: user});
 			if (userAuth) {
